749c84ba1e8fc8230963680507ac40bcb1b4da33
txzps <giap2k51111@gmail.com>
2025-09-29 15:46:42 +0700

    Add project files.

---
+++ ModelStudent/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ModelStudent.Models;
+using System.Collections.Generic;
+using System.Linq;
+using ModelStudent.Models;
+
+namespace ModelStudent.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> listStudents = new List<Student>()
+        {
+            new Student { Id = 1, Name = "Nguyen Van A", Email = "a@gmail.com", Password = "Aa@123456",
+                          Branch = Branch.IT, Gender = Gender.Male, IsRegular = true,
+                          Address = "Hà Nội", DateOfBorth = new DateTime(1998,5,10), Score = 8.5 }
+        };
+
+        public IActionResult Index()
+        {
+            return View(listStudents);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = "IT", Value = "0" },
+                new SelectListItem { Text = "BE", Value = "1" },
+                new SelectListItem { Text = "CE", Value = "2" },
+                new SelectListItem { Text = "EE", Value = "3" }
+            };
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                s.Id = listStudents.Last().Id + 1;
+                listStudents.Add(s);
+                return View("Index", listStudents);
+            }
+
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = "IT", Value = "0" },
+                new SelectListItem { Text = "BE", Value = "1" },
+                new SelectListItem { Text = "CE", Value = "2" },
+                new SelectListItem { Text = "EE", Value = "3" }
+            };
+
+            return View();
+        }
+    }
+}
---
+++ ModelStudent/Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelStudent.Models
+{
+    public enum Branch { IT, BE, CE, EE }
+    public enum Gender { Male, Female, Other }
+
+    public class Student
+    {
+        public int Id { get; set; } // Mã sinh viên
+
+        [Required(ErrorMessage = "Họ tên bắt buộc phải nhập")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Họ tên phải từ 4 đến 100 ký tự")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email bắt buộc phải nhập")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@gmail\.com$", ErrorMessage = "Email phải có đuôi @gmail.com")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu bắt buộc phải nhập")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$",
+            ErrorMessage = "Mật khẩu phải chứa chữ hoa, chữ thường, số và ký tự đặc biệt")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Ngành học bắt buộc phải chọn")]
+        public Branch Branch { get; set; }
+
+        [Required(ErrorMessage = "Giới tính bắt buộc phải chọn")]
+        public Gender Gender { get; set; }
+
+        public bool IsRegular { get; set; } // true: chính quy, false: phi chính quy
+
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Địa chỉ bắt buộc phải nhập")]
+        public string? Address { get; set; }
+
+        [Range(typeof(DateTime), "1/1/1963", "12/31/2005", ErrorMessage = "Ngày sinh phải trong khoảng 1963 đến 2005")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Ngày sinh bắt buộc phải nhập")]
+        public DateTime DateOfBorth { get; set; }
+
+        [Range(0.0, 10.0, ErrorMessage = "Điểm phải nằm trong khoảng từ 0.0 đến 10.0")]
+        [Required(ErrorMessage = "Điểm bắt buộc phải nhập")]
+        public double Score { get; set; }
+    }
+}
